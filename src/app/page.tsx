dcd084e301366a819d80d0d54c42688cac579b1b
maria.duran.bikleek <cuentasti@novahold.com>
2025-01-30 15:38:54 -0500

    Se agrega link a novahold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
-        <Image
-          className="w-full h-auto"
-          width={1200}
-          height={400}
-          src="/estamos_trabajando.png"
-          alt="Estamos trabajando"
-        />
+    <div className="relative">
+      <Image
+        className="w-full h-auto"
+        width={1200}
+        height={400}
+        src="/estamos_trabajando.png"
+        alt="Estamos trabajando"
+      />
+      <Link
+        href="https://www.linkedin.com/authwall?trkInfo=AQG8Sce0ewI52gAAAZS458qQJCGuCr5zKlp46qpS6D5B4WVn1Ebws8wRunHP7I8v9B3n6eDW62uvxtnUXBgHx9MKWJV0Mbz1sCWCsWjmbYMMvquH1Jlj6Nisej6WKZykViUqRu4=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fnovahold-enterprises%2F%3FviewAsMember%3Dtrue" className="absolute inset-0 flex items-center justify-start text-black" target="_blank">
+      </Link>
     </div>
   );
 }
